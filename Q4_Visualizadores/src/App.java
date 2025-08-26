166e5df722a3da046dac6e96c184ba6557073e8a
m-bortolossi333 <m.bortolossi@edu.pucrs.br>
2025-08-26 22:27:47 +0000

    switch implemntado para opcao de visualizador

--- Q4_Visualizadores/src/App.java
+++ Q4_Visualizadores/src/App.java
@@ -9,18 +9,54 @@ public class App {
 
         Scanner s = new Scanner(System.in);
         int valor = 0;
-        while(true){
-            System.out.println("\nEntre um valor positivo maior que zero (0=fim):");
-            valor = Integer.parseInt(s.nextLine());
-            if (valor == 0){
-                break;
+
+        System.out.println("------------------------------------------------------");
+        System.out.println("Selecione a opção desejeda");
+        System.out.println("Opção -1 Visualizar o Somatorio");
+        System.out.println("Opção -2 Visualizar a media");
+        System.out.println("Opção -3 Visualizar a Multiplicacao");
+        System.out.println("Opção 0 Sair");
+        System.out.println("------------------------------------------------------");
+
+        System.out.println("OBS: Selecione a opção primeiro antes do valor");
+
+        while (true) {
+            System.out.println("\nEntre um valor positivo, se deseja sair digite 0):");
+            try {
+                valor = Integer.parseInt(s.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.println("Entrada invalida. Por favor, digite um numero.");
+                continue;
             }
-            dados.add(valor);
-            dados.adicionaIVisualizador(cs);
-            dados.adicionaIVisualizador(cmult);
-            dados.adicionaIVisualizador(cm);
-            
+
+            if (valor == 0) {
+                break;
+            } else if (valor > 0) {
+                System.out.println("--- DADOS ATUALIZADOS ---");
+                dados.add(valor); // A fonte de dados notifica os observadores ATIVOS
+            } else {
+                // Comandos para ligar os observadores
+                switch (valor) {
+                    case -1:
+                        dados.adicionaIVisualizador(cs);
+                        System.out.println("=> Visualizador de SOMATORIO ativado.");
+                        break;
+                    case -2:
+                        dados.adicionaIVisualizador(cm);
+                        System.out.println("=> Visualizador de MEDIA ativado.");
+                        break;
+                    case -3:
+                        dados.adicionaIVisualizador(cmult);
+                        System.out.println("=> Visualizador de MULTIPLICACAO ativado.");
+                        break;
+                    default:
+                        System.out.println("Comando desconhecido.");
+                        break;
+                }
+            }
         }
-        System.out.println("Fim");
+        s.close();
+        System.out.println("Fim do programa.");
     }
+
 }
