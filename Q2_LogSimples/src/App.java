ebb27c6b4f1d31ac1fec02fc492fc7bf0333109f
m-bortolossi333 <m.bortolossi@edu.pucrs.br>
2025-09-03 18:48:51 +0000

    Parte Rafael, não conseguiu dar o pull

--- Q2_LogSimples/src/App.java
+++ Q2_LogSimples/src/App.java
@@ -1,11 +1,24 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        LogSimples log = new LogSimples();
+        LogSimples log = LogSimples.getInstance();
+
         log.log("Mensagem de alerta 1");
         log.log("Mensagem de alarme 23");
         log.log("Mensagem de PANICO!!");
 
-        for(String m:log){
+        for (String m : log) {
+            System.out.println(m);
+        }
+
+        System.out.println("End of first instance \n");
+
+        LogSimples oneAndTheSame = LogSimples.getInstance();
+
+        oneAndTheSame.log("Mensagem de alerta 2");
+        oneAndTheSame.log("Mensagem de alarme 24");
+        oneAndTheSame.log("Mensagem de PANICO!!");
+
+        for (String m : oneAndTheSame) {
             System.out.println(m);
         }
     }
--- Q2_LogSimples/src/LogSimples.java
+++ Q2_LogSimples/src/LogSimples.java
@@ -3,19 +3,30 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
-public class LogSimples implements Iterable<String>{
+public class LogSimples implements Iterable<String> {
+
     private List<String> mensagens;
 
-    public LogSimples(){
+    private static LogSimples instance;
+
+    private LogSimples() {
         mensagens = new LinkedList<>();
     }
 
-    public void log(String m){
+    public static void log(String m) {
         String logM = LocalDate.now().toString() + " : " + m;
-        mensagens.add(logM);
+        instance.mensagens.add(logM);
+    }
+
+    public static LogSimples getInstance() {
+        if (instance == null) {
+            instance = new LogSimples();
+        }
+        return instance;
     }
 
     @Override
     public Iterator<String> iterator() {
         return mensagens.iterator();
     }
+}
